075fbbdf762b490e2c8a970d47faf604160d7a2e
KokoroHoshi <KokoroHoshi2@gmail.com>
2024-05-21 16:05:05 +0800

    uninstall canvasjsstockcharts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,9 @@ import router from "./router";
 
 // install bootstrap
 import "bootstrap/dist/css/bootstrap.css";
-// install @canvasjs/charts
-// https://www.npmjs.com/package/@canvasjs/vue-stockcharts
-import CanvasJSStockChart from "@canvasjs/vue-stockcharts";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(CanvasJSStockChart);
 
 app.mount("#app");
