47aa8376ab08eae638fa128ded1c7270bf2c5731
KokoroHoshi <KokoroHoshi2@gmail.com>
2024-05-25 05:31:20 +0800

    update BottomNavbar

--- frontend/src/components/BottomNavbar.vue
+++ frontend/src/components/BottomNavbar.vue
@@ -1,23 +1,58 @@
 <template>
   <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
     <div class="mx-auto">
-      <ul class="navbar-nav text-center">
+      <ul
+        class="navbar-nav text-center"
+        v-for="(link, index) in links"
+        :key="index"
+      >
         <li>
-          <router-link to="/investment" class="nav-link"
+          <router-link
+            :to="`${link.url}`"
+            class="nav-link"
+            active-class="active"
+            >{{ link.text }}</router-link
+          >
+        </li>
+        <!-- <li>
+          <router-link to="/investment" class="nav-link" active-class="active"
             >Investment</router-link
           >
         </li>
         <li>
-          <router-link to="/record" class="nav-link active">Record</router-link>
+          <router-link to="/record" class="nav-link" active-class="active"
+            >Record</router-link
+          >
         </li>
         <li>
-          <router-link to="/history" class="nav-link">History</router-link>
-        </li>
+          <router-link to="/history" class="nav-link" active-class="active"
+            >History</router-link
+          >
+        </li> -->
       </ul>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        {
+          text: "Investment",
+          url: "/investment",
+        },
+        {
+          text: "Record",
+          url: "/record",
+        },
+        {
+          text: "History",
+          url: "/history",
+        },
+      ],
+    };
+  },
+};
 </script>
