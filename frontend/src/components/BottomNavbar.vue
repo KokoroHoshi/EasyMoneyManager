3a4c378497dff5804dff3ef33d3d6e3547f935fc
KokoroHoshi <KokoroHoshi2@gmail.com>
2024-05-25 05:47:30 +0800

    fix BottomNavbar

--- frontend/src/components/BottomNavbar.vue
+++ frontend/src/components/BottomNavbar.vue
@@ -1,12 +1,8 @@
 <template>
   <nav class="navbar fixed-bottom navbar-expand-lg navbar-dark bg-dark">
     <div class="mx-auto">
-      <ul
-        class="navbar-nav text-center"
-        v-for="(link, index) in links"
-        :key="index"
-      >
-        <li>
+      <ul class="navbar-nav text-center">
+        <li v-for="(link, index) in links" :key="index">
           <router-link
             :to="`${link.url}`"
             class="nav-link"
@@ -14,21 +10,6 @@
             >{{ link.text }}</router-link
           >
         </li>
-        <!-- <li>
-          <router-link to="/investment" class="nav-link" active-class="active"
-            >Investment</router-link
-          >
-        </li>
-        <li>
-          <router-link to="/record" class="nav-link" active-class="active"
-            >Record</router-link
-          >
-        </li>
-        <li>
-          <router-link to="/history" class="nav-link" active-class="active"
-            >History</router-link
-          >
-        </li> -->
       </ul>
     </div>
   </nav>
